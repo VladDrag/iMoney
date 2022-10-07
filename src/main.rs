696cdd8e076138ee53140d1b3253c4373adf3fda
Vlad Dragomir <vdragomir@protonmail.com>
2022-10-07 16:42:05 +0200

    Finish initial models

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,7 @@ mod models;
 fn main() {
     let expense = models::Expense::new(String::from("food"), 1000.0);
 	println!("expenses for {} are {}", expense.get_name(), expense.get_amount());
+
+	let income = models::Income::new(String::from("salary"), 10000.0);
+	println!("income for {} is {}", income.name(), income.amount());
 }
